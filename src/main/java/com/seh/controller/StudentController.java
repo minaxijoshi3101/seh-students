35c33a04dc14e39487c04035a50752a974056cdd
minaxi <minaxijoshi3101@gmail.com>
2023-09-17 13:01:20 +0800

    code baseline

---
+++ src/main/java/com/seh/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.seh.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.seh.model.Student;
+import com.seh.repository.StudentRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/student")
+//@RequestMapping("/api")
+public class StudentController {
+	@Autowired 
+	StudentRepository studentRepo; 
+	
+	@PostMapping("/register")
+    public String enrollStudent(Student student)
+    {
+        studentRepo.save(student);
+        return "Student Enrolled Successfully!!!";
+    }
+	
+	@GetMapping("/students")
+	public ResponseEntity<List<Student>> getAllEmployees() {
+		List<Student> studentList = new ArrayList<>();
+		studentRepo.findAll().forEach(studentList::add);
+		return new ResponseEntity<List<Student>>(studentList, HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/seh/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.seh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.seh.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	//Student findByEmpcity(String emp_city);
+	//Optional<List<Student>> findByEmpageGreaterThan(int emp_age);
+	//Optional<Student> findOneByEmailAndPassword(String email, String password);
+	//Student findByEmail(String email);
+	//String enrollStudent(Student student);
+
+}
