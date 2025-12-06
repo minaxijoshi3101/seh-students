a9f2780f9607af7528c3c969bc591e6ffd36856a
minaxi-esamyak <minaxijoshi3101@gmail.com>
2025-12-07 03:04:54 +0800

    prepared build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+
 ART_URL="https://trialcpnyac.jfrog.io/artifactory"
+
 REPO="SEH"
 ART_USER="amitajoshi1992@gmail.com"
 ART_API_KEY=$1
@@ -9,9 +11,9 @@ mvn clean install -DskipTests=true
 
 zip -r deployment_manifest.zip deployment_manifest/
 
-app_name=$(xmllint -xpath "/*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
+app_name=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='artifactId']/text()" pom.xml)
 
-version=$(xmllint -xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
+version=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml)
 
 JAR_FILE="target/${app_name}-${version}.jar"
 
@@ -23,4 +25,4 @@ curl -u ${ART_USER}:${ART_API_KEY} -T ${JAR_FILE} "${ART_URL}/${REPO}/${app_name
 
 curl -u ${ART_USER}:${ART_API_KEY} -T deployment_manifest.zip "${ART_URL}/${REPO}/${app_name}/release/${version}/deployment_manifest.zip"
 
-echo "Artifacts uploaded successfully."
+echo "Artifact and deployment manifest uploaded successfully."
