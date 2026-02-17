1de9e61f3d78046ee21a98fe35ff6b44fe62a992
minaxi-esamyak <minaxijoshi3101@gmail.com>
2026-02-17 22:19:41 +0800

    logging implemeted

--- src/main/java/com/seh/controller/StudentController.java
+++ src/main/java/com/seh/controller/StudentController.java
@@ -1,45 +1,51 @@
-//Presentation Layer - controllers to accept the requests from the client
 package com.seh.controller;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import com.seh.model.Student;
 import com.seh.repository.StudentRepository;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/student")
-//@RequestMapping("/api")
 public class StudentController {
-	@Autowired 
-	StudentRepository studentRepo; 
-	
-	@PostMapping("/register")
-    public Student enrollStudent(@RequestBody Student student)
-    {
-        studentRepo.save(student);
-        return student;
-    }
-	
-	//Get all the student details
-	@GetMapping("/students")
-	/*public String home() {
-		return "welcome!!";
-	}*/
-	public ResponseEntity<List<Student>> getAllEmployees() {
-		List<Student> studentList = new ArrayList<>();
-		studentRepo.findAll().forEach(studentList::add);
-		return new ResponseEntity<List<Student>>(studentList, HttpStatus.OK);
-	}
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    @Autowired
+    StudentRepository studentRepo;
+
+    @PostMapping("/register")
+    public Student enrollStudent(@RequestBody Student student) {
+
+        logger.info("Received request to register student: {}", student);
+
+        Student savedStudent = studentRepo.save(student);
+
+        logger.info("Student registered successfully with ID: {}", savedStudent.getId());
+
+        return savedStudent;
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllEmployees() {
+
+        logger.info("Received request to fetch all students");
+
+        List<Student> studentList = new ArrayList<>();
+        studentRepo.findAll().forEach(studentList::add);
+
+        logger.info("Returning {} students", studentList.size());
+
+        return new ResponseEntity<>(studentList, HttpStatus.OK);
+    }
 }
